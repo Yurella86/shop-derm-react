ab27f175ce71246e52d637c9c7acf8b2428e35af
Turko Yuriy <yurella1986@gmail.com>
2023-05-19 12:14:16 +0300

    configure mini cart

--- src/components/Header/mini cart.jsx
+++ src/components/Header/mini cart.jsx
@@ -7,27 +7,29 @@ function MiniCart() {
 
     function handleHover() {
         senDropdown(true);
+        console.log(dropdown);
     }
 
     function handleMouseLeave() {
         senDropdown(false);
+        console.log(dropdown);
     }
 
     return (
         <div
-            className='item mini-cart-wrapper'
+            className='mini-cart-wrapper'
             onMouseOver={handleHover}
-            onMouseLeave={handleMouseLeave}>
-            <div className='mini-cart-icon'>cart
-            </div>
-
-            <div
-                className={`mini-cart-dropdown ${dropdown === true ? 'active' : ''}`}>
-                <div className='dropdown'>eeeeeeeeeee
+            onMouseLeave={handleMouseLeave}
+        >
+            <div className={`mini-cart-icon ${dropdown === true ? 'active' : ''}`}>
+                <div
+                    className={`mini-cart-dropdown ${dropdown === true ? 'active' : ''}`}>
+                    <div className='mini-cart-dropdown-content'>
+                        <span>You can see some content</span>
+                    </div>
                 </div>
-                <div className='mini-cart-dropdown-content'>
-                    <span>You can see some content</span>
-                </div>
+                <span className={`icon icon-shopping-cart`}>
+                </span>
             </div>
         </div>
     );
