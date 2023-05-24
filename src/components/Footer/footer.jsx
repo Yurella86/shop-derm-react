12da671cfc0c094dcf883d92a74e3c13d0de1ca6
Turko Yuriy <yurella1986@gmail.com>
2023-05-24 19:09:34 +0300

    created footer primary container (newslatter)

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -4,7 +4,60 @@ import '../../style/extend.scss';
 const Footer = () => {
     return (
         <div className='footer-container'>
-            Footer container
+            <div className='footer-top-container'>
+                <div className='main-container footer-top-flex'>
+                    <div className='item item-left'>
+                        <ul className='links'>
+                            <li>Search Terms</li>
+                            <li>Site Map</li>
+                            <li>Advanced Search</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+                    <div className='item item-right'>Tag line</div>
+                </div>
+            </div>
+            <div className='footer-primary-container'>
+                <div className='main-container'>
+                    <div className='footer-primary'>
+                        <div className='footer-primary-left'>
+                            <div className='socialicons'>
+                                <a href="#!"><span className='icon icon-social-facebook'></span></a>
+                                <a href="#!"><span className='icon icon-social-pinterest'></span></a>
+                                <a href="#!"><span className='icon icon-social-skype'></span></a>
+                                <a href="#!"><span className='icon icon-social-linkedin'></span></a>
+                            </div>
+                        </div>
+                        <div className='footer-primary-right'>
+                            <div className='newsletter-wrapper'>
+                                <div className='content'>
+                                    <label className='label' for="newsletter">
+                                        <span>Sign up for news & special offers!</span>
+                                    </label>
+                                    <div className='field newsletter icon-mail'>
+
+                                        <input type="email" placeholder='Enter Email Address' />
+                                    </div>
+                                    <div className='actions'>
+                                        <button className='action subscribe primary'>
+                                            <span>subscribe</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='footer-secondary'>
+
+                    </div>
+                </div>
+            </div>
+            <div className='footer-secondary-container'>
+                <div className='main-container'>w</div>
+            </div>
+            <div className='footer-bottom-container section-container'>
+                <div className='main-container'>s</div>
+            </div>
         </div>
     );
 };
