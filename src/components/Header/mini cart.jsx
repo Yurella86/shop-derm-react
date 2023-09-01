52cd6db75f829518e9ceff1c221e878954001a57
Turko Yuriy <yurella1986@gmail.com>
2023-09-01 19:21:17 +0300

    resolve conflict

--- src/components/Header/mini cart.jsx
+++ src/components/Header/mini cart.jsx
@@ -1,23 +1,13 @@
-<<<<<<< Updated upstream:src/components/Header/mini cart.jsx
-import React, { useState } from 'react';
-import '../../style/extend.scss';
-import { Link } from 'react-router-dom';
-
-function MiniCart() {
-    const [dropdown, senDropdown] = useState(false)
-=======
 import React, { useContext, useEffect, useState } from 'react';
-import '../../../style/extend.scss';
-import { Link } from 'react-router-dom';
-import CartPopup from './cartPopap';
-import CartContext from '../../../store/cartContext';
+import '../../style/extend.scss';
+import CartPopup from './Mini Cart/cartPopap';
+import CartContext from '../../store/cartContext';
 
 function MiniCart() {
     const [dropdown, setDropdown] = useState(false)
     const [isItem, setIsItem] = useState(false)
     const [counterOfItems, setCounterOfItems] = useState(0)
     const cartCtx = useContext(CartContext)
->>>>>>> Stashed changes:src/components/Header/Mini Cart/mini cart.jsx
 
     useEffect(() => {
         if (cartCtx.items.length > 0) {
@@ -32,22 +22,12 @@ function MiniCart() {
             onMouseOver={() => setDropdown(true)}
             onMouseLeave={() => setDropdown(false)}
         >
-<<<<<<< Updated upstream:src/components/Header/mini cart.jsx
-            <Link to='/cart'>
-                <div className={`mini-cart-icon ${dropdown === true ? 'active' : ''}`}>
-                    <div
-                        className={`mini-cart-dropdown ${dropdown === true ? 'active' : ''}`}>
-                        <div className='mini-cart-dropdown-content'>
-                            <span>You can see some content</span>
-                        </div>
-=======
             <div className={`mini-cart-icon ${dropdown === true ? 'active' : ''}`}>
                 <div
                     className={`mini-cart-dropdown ${dropdown === true ? 'active' : ''}`}>
                     <div className='mini-cart-dropdown-content'>
                         {!isItem && <span>You have no items in cart</span>}
                         {isItem && <CartPopup />}
->>>>>>> Stashed changes:src/components/Header/Mini Cart/mini cart.jsx
                     </div>
                 </div>
                 <div className={`icon icon-shopping-cart`}>
--- src/components/Pages/home.jsx
+++ src/components/Pages/home.jsx
@@ -1,21 +1,10 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import banner from '../../images/banners/banner 4.jpg'
-import productImage from '../../images/products/jeuveau-sku_31.jpg'
-
-const Home = () => {
-=======
 import React, { useState } from 'react';
-import Banners from '../Mine/Banners/banners';
 import ProductTubs from '../Mine/Home Tabs/productTubs';
 
 const Home = () => {
 
-    const [stateTab, setStateTab] = useState('bestsellers')
+    const [stateTab, setStateTab] = useState('bestsellers');
 
-
-
->>>>>>> Stashed changes
     return (
         <div>
             {/* <section className="slide-wrapper">
@@ -35,14 +24,6 @@ const Home = () => {
                 section 2
             </section> */}
 
-<<<<<<< Updated upstream
-            <section className='banners'>
-                <img src={banner} alt="banner" />
-            </section>
-=======
-            <Banners />
-
->>>>>>> Stashed changes
             <section className='main-container horizontal-tab'>
                 <ul className='tabs-nav'>
                     <li className='tabs-state' onClick={() => setStateTab('bestsellers')} ><span>Bestsellers</span></li>
@@ -50,42 +31,7 @@ const Home = () => {
                     <li className='tabs-state' onClick={() => setStateTab('staffFavorites')} ><span>Staff Favorites</span></li>
                 </ul>
                 <hr />
-<<<<<<< Updated upstream
-                <div id='tab-1' className='r-tabs-panel'>
-                    <div className="products-wrapper">
-                        <ul className='items'>
-                            <li className='product-item'>
-                                <div className='product-wrap'>
-                                    <div className='product-item-img'>
-                                        <img src={productImage} alt="productImage" />
-                                        <div className='icons'>
-                                            <div className='wish-list'>
-                                                <span className="icon icon-heart"></span>
-                                            </div>
-                                            <div className='compare'>
-                                                <span className="icon icon-arrow-repeat"></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className='product-item-info'>
-                                        <h3 className='product-title'>Jeuveau® By the Unit</h3>
-                                        <div className='price'>$13.00</div>
-                                        <div className='action primary'>
-                                            <button>add to cart</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                {/* <div id='tab-2' className='r-tabs-panel'>tab-2</div>
-                <div id='tab-3' className='r-tabs-panel'>tab-3</div> */}
-
-
-=======
                 {stateTab && <ProductTubs categoryTub={stateTab} />}
->>>>>>> Stashed changes
             </section>
 
         </div>
