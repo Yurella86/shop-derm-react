7833644998c746617f16755f4eb5963e9617b0ab
Turko Yuriy <yurella1986@gmail.com>
2023-10-02 16:58:39 +0300

    added style for HomeTabs hover and active

--- src/components/Pages/home.jsx
+++ src/components/Pages/home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
             <Banners />
             <section className='main-container horizontal-tab'>
                 <ul className='tabs-nav'>
-                    <li className='tabs-state' onClick={() => setStateTab('bestsellers')} ><span>Bestsellers</span></li>
-                    <li className='tabs-state' onClick={() => setStateTab('monthsSpecials')} ><span>This Month's Specials</span></li>
-                    <li className='tabs-state' onClick={() => setStateTab('staffFavorites')} ><span>Staff Favorites</span></li>
+                    <li className={stateTab === 'bestsellers' ? 'tabs-state active' : 'tabs-state'} onClick={() => setStateTab('bestsellers')} ><span>Bestsellers</span></li>
+                    <li className={stateTab === 'monthsSpecials' ? 'tabs-state active' : 'tabs-state'} onClick={() => setStateTab('monthsSpecials')} ><span>This Month's Specials</span></li>
+                    <li className={stateTab === 'staffFavorites' ? 'tabs-state active' : 'tabs-state'} onClick={() => setStateTab('staffFavorites')} ><span>Staff Favorites</span></li>
                 </ul>
                 <hr />
                 {stateTab && <ProductTubs categoryTub={stateTab} />}
