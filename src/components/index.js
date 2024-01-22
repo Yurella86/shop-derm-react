da00b23dd1e1789b8092e40cd01e8d9649ee041f
Yurella <yurella1986@gmail.com>
2024-01-22 23:55:12 +0200

    created admin page

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,19 @@
 import Header from "./Header/header";
 import Categories from "./Header/categories";
-import MiniCart from './Header/mini cart';
-import Compare from './Header/compare';
-import HeaderNavMenu from './Header/Header Nav Menu/nav menu';
-import Home from './Pages/home';
-import Cart from './Pages/cart';
-import ComparePage from './Pages/ComparePage/comparePage';
-import Services from './Pages/Category/services';
-import Products from './Pages/Category/products';
-import Specials from './Pages/Category/specials';
-import GiftCard from './Pages/Category/gift-card';
-import Account from './Pages/account';
+import MiniCart from "./Header/mini cart";
+import Compare from "./Header/compare";
+import HeaderNavMenu from "./Header/Header Nav Menu/nav menu";
+import Home from "./Pages/home";
+import Cart from "./Pages/cart";
+import ComparePage from "./Pages/ComparePage/comparePage";
+import Services from "./Pages/Category/services";
+import Products from "./Pages/Category/products";
+import Specials from "./Pages/Category/specials";
+import GiftCard from "./Pages/Category/gift-card";
+import Account from "./Pages/account";
 import Footer from "./Footer/footer";
 import Authentication from "./Pages/authentication";
-
+import AdminPanel from "./Pages/adminPanel";
 
 export {
     Header,
@@ -30,5 +30,6 @@ export {
     GiftCard,
     Account,
     Footer,
-    Authentication
+    Authentication,
+    AdminPanel,
 };
