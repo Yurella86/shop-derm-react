1798788ef5d7a2663fe8580e16ace1784a9bf71c
Yurella <yurella1986@gmail.com>
2024-01-23 14:45:43 +0200

    create put data in json file

--- src/components/Pages/adminPanel.jsx
+++ src/components/Pages/adminPanel.jsx
@@ -1,17 +1,54 @@
 import React from "react";
+import useAdminPanelLogic from "./../../store/AdminApi/AdminPanelLogic";
 
 const AdminPanel = () => {
+    const {colors, updateData, getColor} = useAdminPanelLogic();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(e.target.elements.primary.value);
+        console.log(e.target.elements.secondary.value);
+        console.log(e.target.elements.additional.value);
+
+        updateData(colors);
+    };
+
     return (
         <div>
-            <div>
-                <input type="color" id="head" name="head" placeholder="#fff" />
-                <label for="head">Primary</label>
-            </div>
-
-            <div>
-                <input type="color" id="body" name="body" />
-                <label for="body">Secondary</label>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input
+                        type="color"
+                        id="primary"
+                        name="primary"
+                        onChange={getColor}
+                        value={colors.primary}
+                    />
+                    <label htmlFor="primary">Primary</label>
+                </div>
+                <div>
+                    <input
+                        type="color"
+                        id="secondary"
+                        name="secondary"
+                        onChange={getColor}
+                        value={colors.secondary}
+                    />
+                    <label htmlFor="secondary">Secondary</label>
+                </div>
+                <div>
+                    <input
+                        type="color"
+                        id="additional"
+                        name="additional"
+                        onChange={getColor}
+                        value={colors.additional}
+                    />
+                    <label htmlFor="additional">Additional</label>
+                </div>
+                <button type="submit">Save</button>
+            </form>
         </div>
     );
 };
